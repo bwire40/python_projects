818c76e9c0dbae5ca44df8dc1cee81dcedfeab5d
bwire40 <emmanuelbwire80@gmail.com>
2025-02-07 16:45:54 +0300

    first game to practice inputs

---
+++ games/madlibs.py
@@ -0,0 +1,14 @@
+# madlibs game
+# word game to create a story by filling blanck with raandom words
+
+# accept user inputs
+adjective1=input("enter adjective (description) : \n")
+noun1=input("enter a noun (naming word): \n")
+adjective2=input("enter an adjective (description):\n")
+verb1=input("Enter a verb ending with 'ing'")
+adjective3=input("enter an adjective (description):\n")
+
+print(f"Today i went to a {adjective1} zoo.")
+print(f"In an exhibit i saw {noun1}.")
+print(f"{noun1} was {adjective2} and {verb1}.")
+print(f"It was {adjective3}")
