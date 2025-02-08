db6bca01f0312447d5f812559496ab59c19183e9
bwire40 <emmanuelbwire80@gmail.com>
2025-02-08 12:35:03 +0300

    String methods

--- conditionals/excercise_7.py
+++ conditionals/excercise_7.py
@@ -2,24 +2,28 @@
 
 
 # user input
-operator=input("Enter an operator (+,-*,/): \n")
-# checks
-if operator !='+' and operator!="-" and operator!='*' and operator!='/':
-    print(f"{operator} is Invalid!.")
-else:
-    num1=float(input("Enter the first number: \n"))
-    num2=float(input("Enter the second number: \n"))
-    if operator =='+':
-        result=num1+num2
-        print(f"You chose {operator}.\nThe answer is {round(result,2)}")
-    elif operator =='-':
-        result=num1-num2
-        print(f"You chose {operator}.\nThe answer is {round(result,2)}")
-    elif operator =="*":
-        result=num1*num2
-        print(f"You chose {operator}.\nThe answer is {round(result,2)}")
-    elif operator=="/":
-        result=num1/num2
-        print(f"You chose {operator}.\nThe answer is {round(result,2)}")
-    else:
-        print("Try again!")
+# operator=input("Enter an operator (+,-*,/): \n")
+# # checks
+# if operator !='+' and operator!="-" and operator!='*' and operator!='/':
+#     print(f"{operator} is Invalid!.")
+# else:
+#     num1=float(input("Enter the first number: \n"))
+#     num2=float(input("Enter the second number: \n"))
+#     if operator =='+':
+#         result=num1+num2
+#         print(f"You chose {operator}.\nThe answer is {round(result,2)}")
+#     elif operator =='-':
+#         result=num1-num2
+#         print(f"You chose {operator}.\nThe answer is {round(result,2)}")
+#     elif operator =="*":
+#         result=num1*num2
+#         print(f"You chose {operator}.\nThe answer is {round(result,2)}")
+#     elif operator=="/":
+#         result=num1/num2
+#         print(f"You chose {operator}.\nThe answer is {round(result,2)}")
+#     else:
+#         print("Try again!")
+
+# conditional expression. one line if statement
+num =5
+print("positive" if num>0 else "Negative")
